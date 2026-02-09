7b4c382e7f909c89cbffe3f514aec38559b60a13
trandon <163871797+x0trandon@users.noreply.github.com>
2026-02-09 09:24:47 +0700

    Add analyze_audio

--- audio.py
+++ audio.py
@@ -2,18 +2,11 @@ import librosa
 import numpy as np
 
 
-def extract_bass_energy(y, sr, hop_length=512):
-    stft = librosa.stft(y, hop_length=hop_length)
-    magnitudes = np.abs(stft)
-
-    freqs = librosa.fft_frequencies(sr=sr, n_fft=2048)
-    bass_mask = freqs <= 150
-
-    bass_energy = magnitudes[bass_mask].mean(axis=0)
-
-    bass_energy = bass_energy / bass_energy.max()
-
-    return bass_energy
+def analyze_audio(file_path, hop_length=512):
+    y, sr = load_audio(file_path)
+    bass_energy = extract_bass_energy(y, sr, hop_length)
+    fps = sr / hop_length
+    return bass_energy, fps
 
 
 def load_audio(file_path):
@@ -23,3 +16,12 @@ def load_audio(file_path):
     print(f"Samples: {len(y)}")
     return y, sr
 
+
+def extract_bass_energy(y, sr, hop_length=512):
+    stft = librosa.stft(y, hop_length=hop_length)
+    magnitudes = np.abs(stft)
+    freqs = librosa.fft_frequencies(sr=sr, n_fft=2048)
+    bass_mask = freqs <= 150
+    bass_energy = magnitudes[bass_mask].mean(axis=0)
+    bass_energy = bass_energy / bass_energy.max()
+    return bass_energy
