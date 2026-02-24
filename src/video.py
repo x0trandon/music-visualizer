6b5565e45fb569f0010ac56ad71df0f28cf76a88
trandon <163871797+x0trandon@users.noreply.github.com>
2026-02-24 21:13:29 +0700

    Update audio codec to alac

--- src/video.py
+++ src/video.py
@@ -5,4 +5,4 @@ from moviepy import ImageSequenceClip
 def encode_video(frames, fps, audio_path, output_path="output/output.mp4"):
     os.makedirs(os.path.dirname(output_path), exist_ok=True)
     clip = ImageSequenceClip(frames, fps=fps)
-    clip.write_videofile(output_path, audio=audio_path)
+    clip.write_videofile(output_path,audio=audio_path, audio_codec="alac")
